842b2ed0fc6dc9682e4ea27ad1b4f7f98233935f
Kel. Cirangrang 02 <agathalaya24@gmail.com>
2023-10-11 20:39:49 +0700

    menambahkan card di pesan

--- src/component/layout/card/dataPelajaran/card-la.jsx
+++ src/component/layout/card/dataPelajaran/card-la.jsx
@@ -1,15 +1,87 @@
 import React from 'react';
 import { Card } from 'antd';
+
 function CardP() {
-    return (
-      <>
-     <Card style={{ width: "100%",height:"100%",background:"gray" }}>
-    <h3>Produk</h3>
-    <h4>Rumah Parahyangan Kencana</h4>
-    <p>Parken Block A 17 RT 07 RW 13</p>
-  </Card>
-      </>
-    );
-  }
-  
-  export default CardP;
+  const cardStyle = {
+    width: "100%",
+    height: "100%",
+    background: "gray",
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const contentStyle2 = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center", // Mengatur vertikal tengah
+  };
+
+  const cardStyle2 = {
+    width: "100%",
+    height: "100%",
+    background: "gray",
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const contentStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center", // Mengatur vertikal tengah
+  };
+
+  const cardStyle3 = {
+    width: "100%",
+    height: "100%",
+    background: "gray",
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const contentStyle3 = {
+    display: "flex",
+    justifyContent: "flex-end", // Mengatur agar h5 ada di sebelah kanan
+    alignItems: "center", // Mengatur vertikal tengah
+  };
+
+  return (
+    <>
+      <Card style={cardStyle}>
+        <h3>Produk</h3>
+        <div style={contentStyle}>
+          <div>
+            <h4>Rumah Parahyangan Kencana</h4>
+            <p>Parken Block A 17 RT 07 RW 13</p>
+          </div>
+          <div>
+            <h4>1x</h4>
+            <h5>RP.10.012.103</h5>
+          </div>
+        </div>
+      </Card>
+      <Card style={cardStyle2}>
+        <h3>Produk</h3>
+        <div style={contentStyle2}>
+          <div>
+            <h4>Rumah Parahyangan Kencana</h4>
+            <p>Parken Block A 17 RT 07 RW 13</p>
+          </div>
+          <div>
+            <h4>1x</h4>
+            <h5>RP.10.012.103</h5>
+          </div>
+        </div>
+      </Card>
+      <Card style={cardStyle3}>
+        <h3>Total Pembayaran</h3>
+        <div style={contentStyle3}>
+          <div>
+            <h5>20,024,206</h5>
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+}
+
+export default CardP;
